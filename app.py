4b7f18c045280625ed18821efefcba19633e725f
Thienvusimplon <thienvu.quach.simplon@gmail.com>
2022-10-12 16:49:18 +0200

    Commenter le code

--- app.py
+++ app.py
@@ -10,39 +10,45 @@ import pandas as pd
 
 st.title("Detect réseaux")
 
-def load_image(image_file):
+"""def load_image(image_file):
     img = Image.open(image_file)
-    return img
+    return img"""
 
-
+# Chargement des modèles
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True, skip_validation=True ) 
 parseq = torch.hub.load('baudm/parseq', 'parseq', pretrained=True).eval()
 img_transform = SceneTextDataModule.get_transform(parseq.hparams.img_size)
 
+# Upload l'image à traiter
 image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg", "JPG"])
 
-crop_number = 1
-
+# Traitement de l'image lorsqu'elle est chargée
 if image_file != None:
-    predictions = model(load_image(image_file))
-    #st.write(predictions.pandas().xyxy[0])
+    
+    # Recherche des centres d'intérêt + detection de couleur
+    predictions = model(Image.open(image_file))
 
+    # Preparation du csv pour download
     df = pd.DataFrame(predictions.pandas().xyxy[0])
-    index = len(predictions.crop()) - 1
     df = df.rename(columns={"confidence": "confiance couleur", "name": "couleur"})
     df = df.drop([df.columns[5]], axis=1)
     df["x_center"] = (df["xmax"] + df["xmin"]) / 2
     df["y_center"] = (df["ymax"] + df["ymin"]) / 2
 
+    # Affichage des centres d'intérêt détecté
+    index = len(predictions.crop()) - 1
+    crop_number = 1
     for i in range(len(predictions.crop())):     
         label_confidence = predictions.crop()[i]["label"].split() 
         st.subheader(f"Crop {crop_number}")
         st.write("Couleur détecté :", label_confidence[0])
         st.write("Score de confiance couleur: ", label_confidence[1]) 
 
+        # Affichage des centres d'intérêt 
         afficher_crop_img = cv2.cvtColor(predictions.crop()[i]["im"], cv2.COLOR_BGR2RGB)
         st.image(afficher_crop_img)
 
+        # Détection des numéros à partir de nos centres d'intérêt
         detect_number_img = Image.fromarray(predictions.crop()[i]["im"])
         detect_number_img_2 = img_transform(detect_number_img).unsqueeze(0)
         logits = parseq(detect_number_img_2)
@@ -50,17 +56,27 @@ if image_file != None:
         label, confidence = parseq.tokenizer.decode(pred)
         st.write('Détection chiffre = {}'.format(label[0]))
         st.write('Score de confiance du chiffre détecté = {}'.format(np.mean(confidence[0].detach().numpy())))
+
+        # Ajout des colonnes numero et confidence numero pour le csv à download
         df.at[index, "numero"] = label[0]
         df.at[index, "confidence numero"] = np.mean(confidence[0].detach().numpy())
 
         index -= 1
         crop_number += 1
     
+    # Réarangement de l'ordre des colonnes du csv
     df = df[["xmin", "xmax", "ymin", "ymax", "x_center", "y_center", "couleur", "confiance couleur", "numero", "confidence numero"]]
+
+    # Affichage de la photo avec les boundings box
     st.image(Image.fromarray(predictions.render()[0]))
+
+    # Affichage du dataframe
     st.write(df)
+
+    # Effacer le dossier runs
     shutil.rmtree('runs/')
 
+    # Préparation du download du csv
     csv = df.to_csv().encode('utf-8')
     st.download_button(
         label="Download data as CSV",
